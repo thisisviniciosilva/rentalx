57e6668c063c79f5d1e1464695ca1ba79e919f0d
Vinicio Silva <viniciomsilva@outlook.com.br>
2021-03-20 17:57:06 -0300

    Creates doc of the creation category route

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import swaggerSetupFile from "./swagger.json";
 const app = express();
 
 app.use(express.json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSetupFile));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSetupFile));
 app.use(routes);
 
 export default app;
