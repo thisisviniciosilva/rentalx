c8dd9e5fc3520dde03920f7d408600e6a8f53d3e
Vinicio Silva <viniciomsilva@outlook.com.br>
2021-03-20 17:17:04 -0300

    Configure Swagger UI Express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import routes from "./routes";
+
+import swaggerSetupFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSetupFile));
 app.use(routes);
 
 export default app;
